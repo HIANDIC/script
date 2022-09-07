2302eefc6e6372a5f3182aec1f455a59ea02b51b
HIANDIC <leaands25@gmail.com>
2022-09-07 20:31:23 +0200

    comment added

--- comment.sh
+++ comment.sh
@@ -10,3 +10,12 @@ Welcome to the Linux Lessons.
 This lesson is about the shell scripting
 EOF
 
+
+<< multiline-comment
+pwd
+ls
+Everything inside the
+HereDoc body is
+a multiline comment
+multiline-comment
+
