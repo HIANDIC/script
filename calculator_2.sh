09eb56c62503b906298e3c4cfbcd289d5071d59e
HIANDIC <leaands25@gmail.com>
2022-09-08 15:55:14 +0200

    calculator_2.sh modified

--- calculator_2.sh
+++ calculator_2.sh
@@ -6,14 +6,16 @@ read -p "Enter a number : " first
 read -p "Enter a number : " second
 
 # use the numbers given by the users in math operations
-let "sum = $first + $second"
+sum=$(($first + $second))
+echo "sum of the numbers : $sum"
+
+<< commentes
 let "sub = $first - $second"
 let "mul = $first * $second"
 let "div = $first / $second"
 
-# after math operations results will be displayed 
-# let command give results as an integer
 echo "Sum of the numbers = $sum"
 echo "Subtraction of the numbers = $sub"
 echo "Multiplication of the numbers = $mul"
 echo "Division of the numbers = $div"
+commentes
