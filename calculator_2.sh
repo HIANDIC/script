de9daa94a0db20567a436d921ab06913d3d14495
HIANDIC <leaands25@gmail.com>
2022-09-08 15:49:28 +0200

    calculator_2.sh with (()) added.

---
+++ calculator_2.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# in this script we will use (()) paranthesis for the math operations and assign it's result to a variable
+# here taking numbers from user
+read -p "Enter a number : " first
+read -p "Enter a number : " second
+
+# use the numbers given by the users in math operations
+let "sum = $first + $second"
+let "sub = $first - $second"
+let "mul = $first * $second"
+let "div = $first / $second"
+
+# after math operations results will be displayed 
+# let command give results as an integer
+echo "Sum of the numbers = $sum"
+echo "Subtraction of the numbers = $sub"
+echo "Multiplication of the numbers = $mul"
+echo "Division of the numbers = $div"
